72e345318e17777a8c9927ab2e51f49c8bddff8c
Cory Hall <ComebackCory1@gmail.com>
2022-04-17 13:55:33 -0500

    finished routing, have note bugs to fix

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
-const db = ('../../db/db');
+const db = require('../../db/db');
+const createNewNote = require('../../lib/note-taker');
 
-router.get('api/notes', (req, res) => {
-   console.log(db);
-})
+router.get('/notes', (req, res) => {
+   res.json(db);
+});
+
+router.post('/notes', (req, res) => {
+   const newNote = createNewNote(req.body, db);
+   res.json(newNote);
+});
 
 module.exports = router;
